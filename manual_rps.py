d6bd25b2edee80d82b2961e1937bc3e5294375dc
samierouf <142994082+samierouf@users.noreply.github.com>
2023-12-27 22:48:02 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -1,17 +1,18 @@
 import random
 
 
-def get_computer_choice():
+def get_computer_choice(): # function randomly chooses a choice for the computer
     possible_plays = ['rock', 'paper', 'scissors']
     computer_choice = random.choice(possible_plays)
     return computer_choice
 
-def get_user_choice():
+def get_user_choice(): # function that asks for the user to input their choice
     print("Rock, Paper or Scissors")
     user_choice = input('Pick one: ')
+    user_choice = user_choice.lower()
     return user_choice
 
-def get_winner(computer_choice, user_choice):
+def get_winner(computer_choice, user_choice): # compare the user and computers choices to determin winner
     if computer_choice == 'rock' and user_choice == 'rock':
         print('It is a tie')
     elif computer_choice == 'rock' and user_choice == 'paper':
@@ -33,7 +34,7 @@ def get_winner(computer_choice, user_choice):
 
 
 
-def play():
+def play(): # function calls the previous function to play the game
     get_winner(get_computer_choice(), get_user_choice())
 
 
